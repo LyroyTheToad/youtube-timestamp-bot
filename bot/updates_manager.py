373ba647b8f4b6b8a048b43249e19e1e0420e1d9
Lyroy TheToad <lyroy.thetoad@gmail.com>
2021-08-07 11:57:03 +0200

    changes

--- bot/updates_manager.py
+++ bot/updates_manager.py
@@ -20,7 +20,6 @@ def updates_manager(tg_update):
     # Initializing variables to avoid future exceptions
     chat_id = None
     chat_type = None
-    message_id = None
     message_text = None
     message_entities = None
     command = None
@@ -39,7 +38,6 @@ def updates_manager(tg_update):
 
         chat_id = message["chat"]["id"]
         chat_type = message["chat"]["type"]
-        message_id = message["message_id"]
         message_text = message["text"]
         message_entities = message.get("entities", {})
         # Check if the message text contains only the command itself
@@ -62,110 +60,104 @@ def updates_manager(tg_update):
     #
 
     #
-    # If the update comes from a chat
+    # If the update comes from a private chat
     #
-    
-    if chat_id:
+
+    if chat_type == "private":
 
         #
-        # If the update comes from a private chat
+        # If the update is a command
         #
 
-        if chat_type == "private":
+        if command:
 
             #
-            # If the update is a command
+            # Start command
             #
 
-            if command:
-
-                #
-                # Start command
-                #
-
-                if command == "/start":
-                    requests.get(TG_API + "/sendMessage", params={
-                        "chat_id": chat_id,
-                        "text": "Send a message in the format  `<YouTube link\> HH:MM:SS `to get your modified YouTube link\!",
-                        "parse_mode": "MarkdownV2"
-                    })
-
-
-
-
-                #
-                # Help command
-                #
-
-                elif command == "/help":
-                    requests.get(TG_API + "/sendMessage", params={
-                        "chat_id": chat_id,
-                        "text": "You must use the format  `<YouTube link\> HH:MM:SS`\n\nExample:\nhttps://youtu\.be/dQw4w9WgXcQ 2:47\n\n"+
-                                "❗️*IMPORTANT*❗️\nThe bot __DOES NOT__ check if the timestamp overflows the duration of the video\!",
-                        "parse_mode": "MarkdownV2",
-                        "disable_web_page_preview": True
-                    })
-
-
-
-
-                #
-                # About command
-                #
-
-                elif command == "/about":
-                    requests.get(TG_API + "/sendMessage", params={
-                        "chat_id": chat_id,
-                        "text": "This bot was made because you can't copy a link that starts a video at a certain time using the official YouTube mobile app." +
-                                "Please use this bot only if you are from a mobile device and not from Desktop to reduce traffic.\n\n" +
-                                "The creator of this bot is @Lyroy_TheToad, if there are any problems with the bot or you want to request a feature fell free to ask me.1n" +
-                                "You can find the code here https://github.com/LyroyTheToad/youtube-timestamp-bot"
-                    })
-
-
-
-
-
-
-            #
-            # Else if it's a message that follows the correct format (yt_link - HH:MM:SS)
-            #
-
-            elif re.search("^http(?:s?):\/\/(?:www\.)?youtu(?:be\.com\/watch\?v=|\.be\/)([\w\-\_]*)(&(amp;)?‌​[\w\?‌​=]*)?\s[0-5]?\d(:[0-5]\d){0,2}$", message_text):
-                
-                #
-                # Send back modified link
-                #
-
-                # Elaborate message
-                message_text = message_text.split(" ")
-                yt_link = message_text[0]
-                time = re.split(":", message_text[1])
-                if len(time) == 1:
-                    time = int(time[0])
-                elif len(time) == 2:
-                    time = int(time[0]) * 60 + int(time[1])
-                else:
-                    time = int(time[0]) * 24 * 60 + int(time[1]) * 60 + int(time[2])
-
-                # Send response
-                requests.get(TG_API + "/sendMessage", params={
-                    "chat_id": chat_id,
-                    "text": yt_link + "?t=" + str(time)
-                })
-
-            
-
-
-
-
-            #
-            # Else (if the user has sent a random message)
-            #
-
-            else:
+            if command == "/start":
                 requests.get(TG_API + "/sendMessage", params={
                     "chat_id": chat_id,
                     "text": "Send a message in the format  `<YouTube link\> HH:MM:SS `to get your modified YouTube link\!",
                     "parse_mode": "MarkdownV2"
                 })
+
+
+
+
+            #
+            # Help command
+            #
+
+            elif command == "/help":
+                requests.get(TG_API + "/sendMessage", params={
+                    "chat_id": chat_id,
+                    "text": "You must use the format  `<YouTube link\> HH:MM:SS`\n\nExample:\nhttps://youtu\.be/dQw4w9WgXcQ 2:47\n\n"+
+                            "❗️*IMPORTANT*❗️\nThe bot __DOES NOT__ check if the timestamp overflows the duration of the video\!",
+                    "parse_mode": "MarkdownV2",
+                    "disable_web_page_preview": True
+                })
+
+
+
+
+            #
+            # About command
+            #
+
+            elif command == "/about":
+                requests.get(TG_API + "/sendMessage", params={
+                    "chat_id": chat_id,
+                    "text": "This bot was made because you can't copy a link that starts a video at a certain time using the official YouTube mobile app." +
+                            "Please use this bot only if you are from a mobile device and not from Desktop to reduce traffic.\n\n" +
+                            "The creator of this bot is @Lyroy_TheToad, if there are any problems with the bot or you want to request a feature fell free to ask me.1n" +
+                            "You can find the code here https://github.com/LyroyTheToad/youtube-timestamp-bot"
+                })
+
+
+
+
+
+
+        #
+        # Else if it's a message that follows the correct format (yt_link - HH:MM:SS)
+        #
+
+        elif re.search("^http(?:s?):\/\/(?:www\.)?youtu(?:be\.com\/watch\?v=|\.be\/)([\w\-\_]*)(&(amp;)?‌​[\w\?‌​=]*)?\s[0-5]?\d(:[0-5]\d){0,2}$", message_text):
+            
+            #
+            # Send back modified link
+            #
+
+            # Elaborate message
+            message_text = message_text.split(" ")
+            yt_link = message_text[0]
+            time = re.split(":", message_text[1])
+            if len(time) == 1:
+                time = int(time[0])
+            elif len(time) == 2:
+                time = int(time[0]) * 60 + int(time[1])
+            else:
+                time = int(time[0]) * 24 * 60 + int(time[1]) * 60 + int(time[2])
+
+            # Send response
+            requests.get(TG_API + "/sendMessage", params={
+                "chat_id": chat_id,
+                "text": yt_link + "?t=" + str(time)
+            })
+
+        
+
+
+
+
+        #
+        # Else (if the user has sent a random message)
+        #
+
+        else:
+            requests.get(TG_API + "/sendMessage", params={
+                "chat_id": chat_id,
+                "text": "Send a message in the format  `<YouTube link\> HH:MM:SS `to get your modified YouTube link\!",
+                "parse_mode": "MarkdownV2"
+            })
