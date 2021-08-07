fc5aaf295e79d3fea87aac9b97fd2064fd5fbaf1
Lyroy TheToad <lyroy.thetoad@gmail.com>
2021-08-07 13:22:17 +0200

    changes

--- run.py
+++ run.py
@@ -1,5 +1,9 @@
 from bot import PORT, DEBUG, app
+from waitress import serve
 
 if __name__ == "__main__":
     # Start server
-    app.run(host="0.0.0.0", port=PORT, debug=DEBUG)
+    if DEBUG:
+        app.run(host="0.0.0.0", port=PORT, debug=True)
+    else:
+        serve(app, listen="*:" + PORT)
