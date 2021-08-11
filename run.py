46fd585b42623fd03098966c58d35ab12f4f0798
Lyroy TheToad <lyroy.thetoad@gmail.com>
2021-08-11 22:31:53 +0200

    changes

--- run.py
+++ run.py
@@ -11,10 +11,10 @@ if __name__ == "__main__":
     # Ignore updates sent while the bot was off (max limit for "getUpdates" method is 100)
     # It's a really ugly and confusing way of doing it but I couldn't find a better one
     try:
-        tg_response = requests.get(TG_API + "/getUpdates", params={"limit": 100, "timeout": 0}).json()
+        tg_response = requests.get(TG_API + "/getUpdates", params={"limit": 100, "timeout": 2}).json()
         latest_update_id = int(tg_response["result"][0]["update_id"])
         while tg_response["result"]:
-            tg_response = requests.get(TG_API + "/getUpdates", params={"offset": latest_update_id + 1, "limit": 100, "timeout": 0}).json()
+            tg_response = requests.get(TG_API + "/getUpdates", params={"offset": latest_update_id + 1, "limit": 100, "timeout": 2}).json()
             if tg_response["result"]:
                 latest_update_id = int(tg_response["result"][0]["update_id"])
     except IndexError:
